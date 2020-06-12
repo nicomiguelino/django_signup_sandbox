69df30a741a27c1751f82193fa64e0820532fde6
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-12 16:35:32 +0800

    Added a basic user registration form using the built-in UserCreationForm

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, SignupView
 
 app_name = 'main'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('signup/', SignupView.as_view(), name='signup')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.views import View
+
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'main/index.html')
+
+
+class SignupView(View):
+    def get(self, request):
+        form = UserCreationForm(label_suffix='')
+        return render(request, 'main/signup.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('main:index')
